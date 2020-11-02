a59a7ca4e789ccfcdb9af13ee3780bf38cde5238
NAGASAI <66065942+NagaSai6@users.noreply.github.com>
2020-11-02 20:21:35 +0530

    change graph.facebook version to 6 and log the response

--- app/controllers/messengerAPI/webhookController.js
+++ app/controllers/messengerAPI/webhookController.js
@@ -158,13 +158,14 @@ function handleMessage(sender_psid, received_message) {
   
     // Send the HTTP request to the Messenger Platform
     request({
-      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "uri": "https://graph.facebook.com/v6.0/me/messages",
       "qs": { "access_token": PAGE_ACCESS_TOKEN },
       "method": "POST",
       "json": request_body
     }, (err, res, body) => {
       if (!err) {
         console.log('message sent!')
+        console.log(`my message ${response}`);
       } else {
         console.error("Unable to send message:" + err);
       }
