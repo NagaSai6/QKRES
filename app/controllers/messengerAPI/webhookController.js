9bbdd8a9e6788ba7badff2971c4d3d0529066ef1
NAGASAI <66065942+NagaSai6@users.noreply.github.com>
2020-11-04 18:07:39 +0530

    update bot

--- app/controllers/messengerAPI/webhookController.js
+++ app/controllers/messengerAPI/webhookController.js
@@ -254,7 +254,7 @@ let callSendAPIWithTemplate = (sender_psid) => {
 
   request({
       "uri": "https://graph.facebook.com/v6.0/me/messages",
-      "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+      "qs": { "access_token": process.env.PAGE_TOKEN },
       "method": "POST",
       "json": body
   }, (err, res, body) => {
