dc06514f72ad21786ee56564395ecb752d7ca53e
NAGASAI <66065942+NagaSai6@users.noreply.github.com>
2020-11-04 17:58:15 +0530

    update bot

--- app/controllers/messengerAPI/webhookController.js
+++ app/controllers/messengerAPI/webhookController.js
@@ -221,5 +221,51 @@ function handleMessage(sender_psid, message) {
   }
 }
 
+let callSendAPIWithTemplate = (sender_psid) => {
+  // document fb message template
+  // https://developers.facebook.com/docs/messenger-platform/send-messages/templates
+  let body = {
+      "recipient": {
+          "id": sender_psid
+      },
+      "message": {
+          "attachment": {
+              "type": "template",
+              "payload": {
+                  "template_type": "generic",
+                  "elements": [
+                      {
+                          "title": "Looking for trusted services?",
+                          "image_url": "../../../public/images/Qres.logo.png",
+                          "subtitle": "Visit our site",
+                          "buttons": [
+                              {
+                                  "type": "web_url",
+                                  "url": "https://glacial-forest-08091.herokuapp.com/",
+                                  "title": "visit now"
+                              }
+                          ]
+                      }
+                  ]
+              }
+          }
+      }
+  };
+
+  request({
+      "uri": "https://graph.facebook.com/v6.0/me/messages",
+      "qs": { "access_token": process.env.FB_PAGE_TOKEN },
+      "method": "POST",
+      "json": body
+  }, (err, res, body) => {
+      if (!err) {
+          // console.log('message sent!')
+      } else {
+          console.error("Unable to send message:" + err);
+      }
+  });
+};
+
+
 
 module.exports = webhookController
