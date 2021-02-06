77b2f739e4a86d99a49bb6520b00b11185a603c1
NAGASAI <66065942+NagaSai6@users.noreply.github.com>
2021-02-06 19:15:52 +0530

    3d-printing

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('Resources/services/main.js', 'public/js/head1.js').sass('Resources/styles.scss', 'public/CSS/styles.css').sourceMaps();
+mix.js('Resources/services/main.js', 'public/js/head1.js').sass('Resources/styles.scss', 'public/CSS/signup.css').sourceMaps();
 
 // Full API
 // mix.js(src, output);
